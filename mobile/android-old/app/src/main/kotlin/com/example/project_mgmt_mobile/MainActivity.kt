31572e3891b4177f2764cb984b2d61a913adc9fb
GP Dhanush <agprakash406@gmail.com>
2025-12-19 13:41:03 +0530

    removed mobile app

--- mobile/android-old/app/src/main/kotlin/com/example/project_mgmt_mobile/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.project_mgmt_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
-
--- mobile/lib/features/auth/domain/repositories/auth_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/user.dart';
-
-abstract class AuthRepository {
-  Future<Map<String, dynamic>> login(String email, String password);
-  Future<void> logout(String? refreshToken);
-  Future<User?> getCurrentUser();
-  Future<bool> isAuthenticated();
-}
